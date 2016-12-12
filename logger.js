e9cab1e66b9254cf10380c7b79a1c8a3c9e951fb
doron segal <doron2402@gmail.com>
2016-12-12 10:24:25 -0800

    logger

---
+++ logger.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const EventEmitter = require('events');
+const 
+class Logger extends EventEmitter {
+  constructor({
+    protocol = 'http', 
+    host='localhost', 
+    port=80, 
+    path = '/log', 
+    method = 'POST',
+    threshold = 10
+  }) {
+    super();
+    this.protocol = protocol;
+    this.host = host;
+    this.port = port;
+    this.path = path;
+    this.method = method;
+  }
+}
+
+const eventInstance = new Logger();
+eventInstance.on('log', (body) => {
+  if (this.protocol === 'https') {
+    
+  } else {
+
+  }
+});
+
+
+module.exports = eventInstance;
+
